c89325f075408bb7c6fef5de648be57062c87db5
John Kenneth Ferrer <ferrejo2@oocl.com>
2018-11-23 11:21:14 +0800

    - Refactor unit test

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -23,6 +23,7 @@ public class TripServiceTest {
 	@Before
 	public void initialize(){
 		tripService = new TestableTripService();
+		loggedInUser = REGISTERED_USER;
 	}
 
 	@Test(expected = UserNotLoggedInException.class)
@@ -36,8 +37,6 @@ public class TripServiceTest {
 
 	@Test
 	public void should_not_return_any_trips_when_users_are_not_friends(){
-		loggedInUser = REGISTERED_USER;
-
 		User friend = new User();
 		friend.addFriend(ANOTHER_USER);
 		friend.addTrip(TO_BRAZIL);
@@ -50,7 +49,6 @@ public class TripServiceTest {
 
 	@Test
 	public void should_not_return_any_trips_when_users_are_friends(){
-		loggedInUser = REGISTERED_USER;
 
 		User friend = new User();
 		friend.addFriend(ANOTHER_USER);
