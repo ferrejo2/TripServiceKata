0479011a5cad5bddbfd9f17de70a510c40a389d9
John Kenneth Ferrer <ferrejo2@oocl.com>
2018-12-13 11:41:50 +0800

    - Refactor TripService - Mock TripDao

--- src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -6,27 +6,31 @@ import java.util.List;
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class TripService {
 
-    public List<Trip> getTripsByUser(User user, User loggedInUser) throws UserNotLoggedInException {
-        if (getLoggedInUser() == null) {
+    @Autowired
+    private TripDAO tripDAO;
+
+    public List<Trip> getFriendTrips(User friend, User loggedInUser) throws UserNotLoggedInException {
+        validate(loggedInUser);
+
+        return friend.isFriendsWith(loggedInUser) ? tripsBy(friend) : noTrips();
+    }
+
+    protected void validate(User loggedInUser) {
+        if (loggedInUser == null) {
             throw new UserNotLoggedInException();
         }
-
-        return  user.isFriendsWith(getLoggedInUser()) ? tripsBy(user) : noTrips();
     }
 
     protected ArrayList<Trip> noTrips() {
         return new ArrayList<Trip>();
     }
 
-    protected List<Trip> tripsBy(User user) {
-        return TripDAO.findTripsByUser(user);
-    }
-
-    protected User getLoggedInUser() {
-        return UserSession.getInstance().getLoggedUser();
+    private List<Trip> tripsBy(User user) {
+        return tripDAO.tripsBy(user);
     }
 
 }
--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -2,49 +2,48 @@ package org.craftedsw.tripservicekata.trip;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.List;
 
 import static org.craftedsw.tripservicekata.trip.UserBuilder.aUser;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.BDDMockito.given;
 
+@RunWith(MockitoJUnitRunner.class)
 public class TripServiceTest {
+
     private static final User GUEST = null;
     private static final User UNUSED_USER = null;
     private static final Trip TO_LONDON = new Trip();
-    private User loggedInUser;
     private static final User REGISTERED_USER = new User();
     private static final User ANOTHER_USER = new User();
     private static final Trip TO_BRAZIL = new Trip();
-    private TripService tripService;
 
-    @Before
-    public void initialize() {
-        tripService = new TestableTripService();
-        loggedInUser = REGISTERED_USER;
-    }
+    @Mock
+    private TripDAO tripDAO;
+
+    @InjectMocks
+    @Spy
+    private TripService tripService = new TripService();
 
     @Test(expected = UserNotLoggedInException.class)
     public void should_throw_an_exception_when_user_is_not_logged_in() {
-//	    TripService tripService = new TestableTripService();
 
-        loggedInUser = GUEST;
-
-        tripService.getTripsByUser(UNUSED_USER, null);
+        tripService.getFriendTrips(UNUSED_USER, GUEST);
     }
 
     @Test
     public void should_not_return_any_trips_when_users_are_not_friends() {
         User friend = aUser().friendsWith(ANOTHER_USER).withTrips(TO_BRAZIL).build();
 
-//		User friend = new User();
-//		friend.addFriend(ANOTHER_USER);
-//		friend.addTrip(TO_BRAZIL);
-
-        List<Trip> friendTrips = tripService.getTripsByUser(friend, null);
+        List<Trip> friendTrips = tripService.getFriendTrips(friend, REGISTERED_USER);
 
         assertThat(friendTrips.size(), is(0));
 
@@ -52,31 +51,13 @@ public class TripServiceTest {
 
     @Test
     public void should_not_return_any_trips_when_users_are_friends() {
-        User friend = aUser().friendsWith(ANOTHER_USER, loggedInUser).withTrips(TO_BRAZIL, TO_LONDON).build();
+        User friend = aUser().friendsWith(ANOTHER_USER, REGISTERED_USER).withTrips(TO_BRAZIL, TO_LONDON).build();
 
-//		User friend = new User();
-//		friend.addFriend(ANOTHER_USER);
-//		friend.addFriend(loggedInUser);
-//		friend.addTrip(TO_BRAZIL);
-//		friend.addTrip(TO_LONDON);
+        given(tripDAO.tripsBy(friend)).willReturn(friend.trips());
 
-        List<Trip> friendTrips = tripService.getTripsByUser(friend, null);
+        List<Trip> friendTrips = tripService.getFriendTrips(friend, REGISTERED_USER);
 
         assertThat(friendTrips.size(), is(2));
 
-    }
-
-
-    private class TestableTripService extends TripService {
-
-        @Override
-        protected User getLoggedInUser() {
-            return loggedInUser;
-        }
-
-        @Override
-        protected List<Trip> tripsBy(User user) {
-            return user.trips();
-        }
     }
 }
